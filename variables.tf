83e48f739733b7285e9dfcfac324795f415ba627
lokeshnagineni <102715389+lokeshnagineni@users.noreply.github.com>
2024-06-04 22:41:04 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,12 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-variable "aws_region" {}
+variable "aws_access_key" {
+    default = "AKIAZI2LH7QOSEYG3WFG"
+}
+variable "aws_secret_key" {
+    default = "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"
+}
+variable "aws_region" {
+    default = "us-east-2"
+}
 variable "vpc_cidr" {}
 variable "vpc_name" {}
 variable "environment" { default = "dev" }
