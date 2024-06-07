fc737d1c4640adb28fc900b13e83a08704b1c68f
lokeshnagineni <102715389+lokeshnagineni@users.noreply.github.com>
2024-06-07 20:31:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ resource aws_vpc vpc {
   )
 }
 
-resource aws_vpc vpc {
+resource aws_vpc vpc_2 {
   cidr_block           = "10.2.0.0/16"
   enable_dns_hostnames = "true"
   enable_dns_support   = "true"
