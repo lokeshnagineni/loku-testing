508a6c66f53c87f00ff785dda390546d02f58ee1
lokeshnagineni <102715389+lokeshnagineni@users.noreply.github.com>
2024-06-11 10:36:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,18 +30,3 @@ resource aws_vpc vpc {
   )
 }
 
-resource aws_vpc vpc_2 {
-  cidr_block           = "10.2.0.0/16"
-  enable_dns_hostnames = "true"
-  enable_dns_support   = "true"
-  instance_tenancy     = "default"
-
-  assign_generated_ipv6_cidr_block = "true"
-
-  tags = merge(
-    tomap({
-      "Name" = "testing-jenkins-2"
-    }),
-    var.tags
-  )
-}
